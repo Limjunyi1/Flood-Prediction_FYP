9ec5b9b1d1d6740b14e2ff447c7f95993d943e77
ACTP2002 <tpcheng_2002@outlook.com>
2024-04-24 18:26:11 +0800

    Update PredictionModelAPI.py
    Fixed importing GetResult bug

--- PredictionModelAPI.py
+++ PredictionModelAPI.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
-from GetResult import get_prediction_result  # Import your prediction model implementation
+from GetResult import GetResult # Import your prediction model implementation
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    getResult = GetResult()
     data = request.json  # Assuming JSON data is sent in the request body
     city = data['city']  # Extract the city from the JSON data
     date = data['date']  # Extract the date from the JSON data
-    prediction = get_prediction_result(city, date) # Call your prediction model function with city and date
+    prediction = getResult.get_prediction_result(city, date) # Call your prediction model function with city and date
     
     return jsonify(prediction)
 
